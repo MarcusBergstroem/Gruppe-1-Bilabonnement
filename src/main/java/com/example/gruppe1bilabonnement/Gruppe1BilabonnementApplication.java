8f38f251a0c1ddbf22ee785d255d5e5fd22d53bd
Adrian <adgo0001@stud.kea.dk>
2024-12-04 09:50:37 +0100

    Initial commit

---
+++ src/main/java/com/example/gruppe1bilabonnement/Gruppe1BilabonnementApplication.java
@@ -0,0 +1,13 @@
+package com.example.gruppe1bilabonnement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gruppe1BilabonnementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gruppe1BilabonnementApplication.class, args);
+	}
+
+}
