ac499a256017c365d6ce5155282ec33f73683ec0
FrederikNngel <fred456d@stud.kea.dk>
2024-12-04 12:07:22 +0100

    test

--- src/main/java/com/example/gruppe1bilabonnement/Controllers/HomeController.java
+++ src/main/java/com/example/gruppe1bilabonnement/Controllers/HomeController.java
@@ -3,6 +3,7 @@ package com.example.gruppe1bilabonnement.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.service.annotation.GetExchange;
 
 @Controller
 public class HomeController {
@@ -10,6 +11,11 @@ public class HomeController {
     @GetMapping("/")
     public String index(Model model) {
         //model.addAttribute("madvarer", carService.fetchAll());
+        System.out.println("soerenbanjomus");
+        return "home/index";
+    }
+    @GetMapping("/createBanjomus")
+    public String createVirksomhed(){
         return "home/index";
     }
 }
