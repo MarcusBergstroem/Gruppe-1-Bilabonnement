e4d4d0be9e4ff99fe541e68a35b8b76fd7eda7aa
FrederikNngel <fred456d@stud.kea.dk>
2024-12-10 15:47:05 +0100

    Fixede så geographyID er unikt

--- src/main/java/com/example/gruppe1bilabonnement/Repository/RenterRepo.java
+++ src/main/java/com/example/gruppe1bilabonnement/Repository/RenterRepo.java
@@ -2,6 +2,7 @@ package com.example.gruppe1bilabonnement.Repository;
 
 import com.example.gruppe1bilabonnement.Model.Renter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -31,17 +32,30 @@ public class RenterRepo {
     }
 
     public int addGeography(String country, String city, String zipCode) {
-        String sql = "INSERT INTO geography (Country, City, ZipCode) VALUES (?, ?, ?)";
+        String selectSql = "SELECT ID FROM geography WHERE Country = ? AND City = ? AND ZipCode = ?";
+        Integer existingId = null;
+        try {
+            existingId = template.queryForObject(selectSql, new Object[]{country, city, zipCode}, Integer.class);
+        } catch (EmptyResultDataAccessException e) {
+            // Hvis den ikke returnere noget kaster den denne exception
+        }
+        if (existingId != null) {
+            return existingId; // Return the existing ID
+        }
+        // Insert a new geography record
+        String insertSql = "INSERT INTO geography (Country, City, ZipCode) VALUES (?, ?, ?)";
         KeyHolder keyHolder = new GeneratedKeyHolder();
         template.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement ps = connection.prepareStatement(insertSql, Statement.RETURN_GENERATED_KEYS);
             ps.setString(1, country);
             ps.setString(2, city);
             ps.setString(3, zipCode);
             return ps;
         }, keyHolder);
-        return keyHolder.getKey().intValue();
+
+        return keyHolder.getKey().intValue(); // Return the newly generated ID
     }
+
     public void addRenter(Renter r, int geographyId) {
         String sql = "INSERT INTO renter (GeographyID, CPR, FirstName, LastName, Address, Blacklist, PhoneNumber) " +
                 "VALUES (?, ?, ?, ?, ?, ?, ?)";
