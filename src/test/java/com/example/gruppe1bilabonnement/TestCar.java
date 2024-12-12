ea73e1376579631d6e73924f924ca564ef2f5efc
Ali <alku0001@stud.kea.dk>
2024-12-12 18:51:46 +0100

    All bildetaljer og alle kontraktdetaljer

--- src/test/java/com/example/gruppe1bilabonnement/TestCar.java
+++ src/test/java/com/example/gruppe1bilabonnement/TestCar.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestCar {
     @Test
     void testGetBrand() {
-        Car car = new Car(10, 3, "Toyota", "Corolla", "Luxury", "1029384", "HS05934", "01-04-2025", "available");
+        Car car = new Car(10, 3, "Toyota", "Corolla", "Luxury", "1029384", "01-04-2025", "available");
         String brandName = car.getCarBrand();
         assertEquals("Toyota", brandName);
     }
