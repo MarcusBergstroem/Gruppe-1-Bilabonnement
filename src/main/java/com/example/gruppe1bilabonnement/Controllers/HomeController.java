6014f12ec39294e6055208114228d93e24c3a226
FrederikNngel <fred456d@stud.kea.dk>
2024-12-18 15:45:08 +0100

    Diverse små fix

--- src/main/java/com/example/gruppe1bilabonnement/Controllers/HomeController.java
+++ src/main/java/com/example/gruppe1bilabonnement/Controllers/HomeController.java
@@ -127,7 +127,7 @@ public class HomeController {
         return "home/udlejede_biler";
     }
 
-    //Laver en en lejekontrakt vja.
+    //Laver en lejekontrakt
     @GetMapping("/opret_lejekontrakt")
     public String createRentalContractForm(Model model) {
 
